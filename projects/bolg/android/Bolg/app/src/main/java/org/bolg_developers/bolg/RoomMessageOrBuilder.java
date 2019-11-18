c18372c7216028ca467da321b1bc74b1770bc986
KokiMatsumura <matsumura.koki1@gmail.com>
2019-11-19 08:57:20 +0900

    add joinroom rpc

--- projects/bolg/android/Bolg/app/src/main/java/org/bolg_developers/bolg/RoomMessageOrBuilder.java
+++ projects/bolg/android/Bolg/app/src/main/java/org/bolg_developers/bolg/RoomMessageOrBuilder.java
@@ -33,5 +33,44 @@ public interface RoomMessageOrBuilder extends
    */
   org.bolg_developers.bolg.CreateAndJoinRoomResponseOrBuilder getCreateAndJoinRoomRespOrBuilder();
 
+  /**
+   * <code>.bolg.JoinRoomRequest join_room_req = 3;</code>
+   */
+  boolean hasJoinRoomReq();
+  /**
+   * <code>.bolg.JoinRoomRequest join_room_req = 3;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomRequest getJoinRoomReq();
+  /**
+   * <code>.bolg.JoinRoomRequest join_room_req = 3;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomRequestOrBuilder getJoinRoomReqOrBuilder();
+
+  /**
+   * <code>.bolg.JoinRoomResponse join_room_resp = 4;</code>
+   */
+  boolean hasJoinRoomResp();
+  /**
+   * <code>.bolg.JoinRoomResponse join_room_resp = 4;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomResponse getJoinRoomResp();
+  /**
+   * <code>.bolg.JoinRoomResponse join_room_resp = 4;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomResponseOrBuilder getJoinRoomRespOrBuilder();
+
+  /**
+   * <code>.bolg.JoinRoomMessage join_room_msg = 5;</code>
+   */
+  boolean hasJoinRoomMsg();
+  /**
+   * <code>.bolg.JoinRoomMessage join_room_msg = 5;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomMessage getJoinRoomMsg();
+  /**
+   * <code>.bolg.JoinRoomMessage join_room_msg = 5;</code>
+   */
+  org.bolg_developers.bolg.JoinRoomMessageOrBuilder getJoinRoomMsgOrBuilder();
+
   public org.bolg_developers.bolg.RoomMessage.DataCase getDataCase();
 }
--- projects/bolg/server/pkg/bolg/streams_manager.go
+++ projects/bolg/server/pkg/bolg/streams_manager.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bolg-developers/x/projects/bolg/server/pb"
 )
 
+// TODO: playerの属性によってフィルターできるようにする
 type streamsManager struct {
 	sync.Mutex
 	streamsMap map[int64]streams
@@ -28,15 +29,30 @@ func (sm *streamsManager) createStreams(roomID int64) error {
 	return nil
 }
 
-func (sm *streamsManager) appendStream(roomID int64, stream pb.BolgService_CreateAndJoinRoomServer) error {
+func (sm *streamsManager) appendStream(roomID int64, stream pb.BolgService_ConnectServer) error {
 	sm.Lock()
 	defer sm.Unlock()
-	streams, ok := sm.streamsMap[roomID]
+	_, ok := sm.streamsMap[roomID]
 	if !ok {
 		return fmt.Errorf("streamsMap[%d] is not found", roomID)
 	}
-	streams = append(streams, stream)
+	sm.streamsMap[roomID] = append(sm.streamsMap[roomID], stream)
 	return nil
 }
 
-type streams []pb.BolgService_CreateAndJoinRoomServer
+func (sm *streamsManager) Broadcasts(roomId int64, ignore pb.BolgService_ConnectServer, msg *pb.RoomMessage) streams {
+	sm.Lock()
+	defer sm.Unlock()
+	streams := make(streams, 0, len(sm.streamsMap[roomId]))
+	for _, s := range sm.streamsMap[roomId] {
+		if s == ignore {
+			continue
+		}
+		if err := s.Send(msg); err != nil {
+			streams = append(streams, s)
+		}
+	}
+	return streams
+}
+
+type streams []pb.BolgService_ConnectServer
