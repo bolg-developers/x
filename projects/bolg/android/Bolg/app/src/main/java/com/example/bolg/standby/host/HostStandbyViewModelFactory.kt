9b366561e5472ec30654f313e98d6c8e3ac17e93
deodorantpower <hasegawa.yuta4@gmail.com>
2019-11-18 11:52:48 +0900

    HostStandbyViewModelFactoryの追加

--- projects/bolg/android/Bolg/app/src/main/java/com/example/bolg/standby/host/HostStandbyViewModelFactory.kt
+++ projects/bolg/android/Bolg/app/src/main/java/com/example/bolg/standby/host/HostStandbyViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.example.bolg.standby.host
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.bolg.standby.player.PlayerStandbyViewModel
 
 class HostStandbyViewModelFactory (private val application: Application): ViewModelProvider.Factory{
     @Suppress("unchecked_cast")
